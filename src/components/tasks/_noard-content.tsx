cf62a139f0493e2893a698b04a7e2ffce533e916
Kingshuk3006 <kingsarkar3006@gmail.com>
2025-11-09 18:45:50 +0530

    task manager layout added

--- src/components/tasks/_noard-content.tsx
+++ src/components/tasks/_noard-content.tsx
@@ -1,16 +1,17 @@
 "use client";
 import React from "react";
 
-import TaskBoardEmployee from "@/components/tasks/task-board-employee";
 import TaskBoardAdmin from "@/components/tasks/task-board-admin";
 import { useSearchParams } from "next/navigation";
+import TaskManagerLayout from "@/app/task-manager/shared-layout";
+import { TaskBoardEmployee } from "./task-board-employee";
 
 const TaskBoardPage = () => {
   const searchParams = useSearchParams();
   const currentIsAdmin = searchParams.get("isAdminView") === "true";
 
-  if (currentIsAdmin) return <TaskBoardAdmin />;
-  else return <TaskBoardEmployee />;
+  if (currentIsAdmin) return <TaskManagerLayout><TaskBoardAdmin /></TaskManagerLayout>;
+  else return <TaskManagerLayout><TaskBoardEmployee /></TaskManagerLayout>;
 };
 
 export default TaskBoardPage;
